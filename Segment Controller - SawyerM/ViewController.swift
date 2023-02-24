f8b151d2f4538b9a688b5f499a39311378fc9b8d
Sawyer Mallonee <temple77@comcast.net>
2023-02-24 10:11:47 -0700

    MVP

--- Segment Controller - SawyerM/ViewController.swift
+++ Segment Controller - SawyerM/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    
+    @IBOutlet weak var Graphic: UIImageView!
+    
+    @IBOutlet weak var segOut: UISegmentedControl!
+    
+    
+    @IBAction func segOut(_ sender: Any) {
+        
+        switch segOut.selectedSegmentIndex
+        {
+        case 0:
+            Graphic.image = UIImage(named:"Gold")
+        case 1:
+            Graphic.image = UIImage(named:"Silver")
+        case 2:
+            Graphic.image = UIImage(named:"Ruby")
+        default:
+            Graphic.image = UIImage(named:"Gold")
+        }
+    }
+    
+    
 }
 
